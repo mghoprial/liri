2854d5d2b96b632f20ea78ea00b2ff98435eb6d3
mghoprial <50455586+mghoprial@users.noreply.github.com>
2019-07-11 08:56:33 -0700

    modified spotify results

--- liri.js
+++ liri.js
@@ -57,35 +57,52 @@ function getSpotify(songName) {
     spotify
         .search({ type: 'track', query: songName })
         .then(function (response) {
-            console.log(response.tracks.items[0].album.artists[0]);
+            // console.log("\n----------\n");
+            // console.log(JSON.stringify (response.tracks,null,2));
+            //console.log(response.tracks);
+            // console.log("\n----------\n");
+            // console.log('\n*******\n', response.tracks.items[0], '\n*******\n');
+            for (var i = 0; i < response.tracks.items.length; i++) {
+
+                // Artist Name
+                console.log("artist name " + response.tracks.items[i].artists[0].name);
+                // Song Name
+                console.log("song title :" + response.tracks.items[i].name);
+
+                // Preview link
+                console.log("preview " + response.tracks.items[i].artists[0].external_urls.spotify);
+                // Album Name
+                console.log("album name :" + response.tracks.items[i].album.name);
+            }
+
+
         })
         .catch(function (err) {
             console.log(err);
         });
 }
 
-function getMovie(movieName)
-{
+function getMovie(movieName) {
 
     var movieURL = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=b3c0b435"
-    
+
     axios.get(movieURL)
-    .then(function (response) {
-        // handle success
-        console.log("title: " + response.data.Title);
-        console.log("year: " + response.data.Year);
-        console.log("ratings: " + response.data.Ratings);
-        //ratings only show [object]
-        console.log("country: " + response.data.Country);
-        console.log("language: " + response.data.Language);
-        console.log("Actors: " + response.data.Actors);
-        console.log("Plot: " + response.data.Plot);
-        //want to clean up above code.
-    })
-    .catch(function (error) {
-      
-        console.log(error);
-        //need to output Mr. Nobody if user doesn't type in movie name
-    })
-   
+        .then(function (response) {
+            // handle success
+            console.log("title: " + response.data.Title);
+            console.log("year: " + response.data.Year);
+            console.log("ratings: " + JSON.stringify(response.data.Ratings));
+            //ratings only show [object]
+            console.log("country: " + response.data.Country);
+            console.log("language: " + response.data.Language);
+            console.log("Actors: " + response.data.Actors);
+            console.log("Plot: " + response.data.Plot);
+            //want to clean up above code.
+        })
+        .catch(function (error) {
+
+            console.log(error);
+            //need to output Mr. Nobody if user doesn't type in movie name
+        })
+
 }
